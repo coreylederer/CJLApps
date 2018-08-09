19d060062681f444013f883149b63bc1a2d74cd7
Corey Lederer <32078160+coreylederer@users.noreply.github.com>
2018-08-08 21:55:56 -0700

    Update blockchain.js
    Block Checking

--- blockchain.js
+++ blockchain.js
@@ -4,8 +4,8 @@
 //
 const SHA256 = require('crypto-js/sha256');
 
-class Block{
-    constructor(block, timestamp, reversible, data, previousHash = ''){
+class Block {
+    constructor(block, timestamp, reversible, data, previousHash = '') {
         this.block = block;
         this.timestamp = timestamp;
         this.reversible = reversible;
@@ -14,7 +14,7 @@ class Block{
         this.currentHash = this.calculateHash();
     }
 
-    calculateHash(){
+    calculateHash() {
         return SHA256(this.block + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
@@ -25,33 +25,57 @@ class Blockchain {
 
     }
 
-    createGenesisBlock(){
-        return new Block("-1", "08/08/2018", "Corey's Genesis Block", "Genesis Block","null previous hash - genesis block");
+    createGenesisBlock() {
+        return new Block("-1", "08/08/2018", "Corey's Genesis Block", "Genesis Block", "null previous hash - genesis block");
     }
 
     getLatestBlock() {
-        return this.chain[this.chain.length -1 ];
+        return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock){
+    addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().currentHash;
         newBlock.currentHash = newBlock.calculateHash();
         this.chain.push(newBlock);
+    }
 
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.currentHash !== currentBlock.calculateHash()) {
+                //console.log('current block hash', currentBlock.hash);
+                console.log(currentBlock.currentHash);
+                console.log('Current block hash !== calculated hash');
+                //return false;
+            } else {
+                console.log('Current Block Hash: ', currentBlock.currentHash, 'Calculated it is: ', currentBlock.calculateHash());
+            }
+
+            if (currentBlock.previousHash !== previousBlock.currentHash) {
+                console.log('Current block hash !== Prev block hash');
+                return false;
+            } else {
+                console.log('Current Block Previous Hash: ', currentBlock.previousHash, 'Previous Block Current Hash: ', previousBlock.currentHash);
+            }
+        }
+        return true;
     }
 }
 
 let coreyCoin = new Blockchain();
-for (iLoop = 0; iLoop < 10;) {
-    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", {amount: iLoop++,stuff: "Things"}));
-    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", {amount: iLoop++,other_stuff: "This other stuff"}));
-    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", {amount: iLoop++,other_stuff_er: "This otherer stuff"}));
-    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", {amount: iLoop++,not_stuff: "This isn't stuff"}));
-    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", {amount: iLoop++,your_stuff: "This is your stuff"}));
-    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "", {amount: iLoop++,boring_stuff: "Boring stuff"}));
-    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "", {amount: iLoop++,sleepy_stuff: "This other stuff will put you to sleep"}));
-    
+for (let iLoop = 0; iLoop < 5;) {
+    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", { amount: iLoop++, stuff: "Things" }));
+    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", { amount: iLoop++, other_stuff: "This other stuff" }));
+    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", { amount: iLoop++, other_stuff_er: "This otherer stuff" }));
+    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", { amount: iLoop++, not_stuff: "This isn't stuff" }));
+    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "no", { amount: iLoop++, your_stuff: "This is your stuff" }));
+    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "", { amount: iLoop++, boring_stuff: "Boring stuff" }));
+    coreyCoin.addBlock(new Block(iLoop, "8/8/2018", "", { amount: iLoop++, sleepy_stuff: "This other stuff will put you to sleep" }));
+
 }
 
+console.log('Is Blockchain Valid?' + coreyCoin.isChainValid());
 
 console.log(JSON.stringify(coreyCoin, null, 3));
